e88f72b74218d527ea3941dae644ae0c6f9867fd
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-03-30 00:54:31 -0600

    added some standard html and started trying to work the login functionality, updated controllers to post and delete routes that may or may not work

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const routes = require('controllers');
+const { strict } = require('assert');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
@@ -12,30 +13,37 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: process.env.SESSION_SECRET,
-    cookie: {
-        maxAge: 60 * 60 * 1000,
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict'
-    },
-    rolling: true,
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-    }),
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+      maxAge: 60 * 60 * 1000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict'
+  },
+  rolling: true,
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+      db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`\nServer running on port ${PORT}`)
-    });
+  app.listen(PORT, () =>
+      console.log(`\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`)
+  );
 });
+
