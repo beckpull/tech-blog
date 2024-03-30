1afe504b02484bea0eed52951465336af897ff1a
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-03-29 20:31:22 -0600

    added auth helper and started seed.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('controllers');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
 const app = express();
